46f31b15fb878a48cb2c3cfd5a8dd1bcb872ac75
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-10-19 17:58:40 -0700

    airbnb clone, initial init

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <img className='header__icon' src="https://d3ui957tjb5bqd.cloudfront.net/uploads/2015/09/airbnb-2.jpg" alt=''/>
+
+    <div className='header__center'>
+      <input type='text' />
+    </div>
+
+    <div className='header__right'>
+      <p>Become a host</p>
+      {/* import icons here. Example language, expand more and avatar icons. */}
+      
+    </div>
+    </div>
+
+    
+  )
+}
+
+export default Header;
